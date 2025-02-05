a5ad3b7db3e18ad5f055c88b4f1707cd1b8ab3c0
s-tirmizi <stirmizi@utexas.edu>
2025-02-04 20:49:12 -0600

    vars

---
+++ variables.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int length = 4;
+    int width = 5;
+    int area;
+
+    // calculate area of rectangle
+    area = length * width;
+
+    // Print
+    cout << "Length: " << length <<"\n";
+    cout << "Widht: " << width << "\n";
+    cout << "Area of the rectangle: " << area << "\n";
+    return 0;
+}
